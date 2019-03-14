7c50e5f4e3c86d1928f140ce0aaed3b0e119c193
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-14 02:17:30 +0000

    Deploy bhuh12/vue-router-tab to github.com/bhuh12/vue-router-tab.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -21,11 +21,11 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "e3a497d9448655a7579981419fd47c86"
+    "revision": "74a1fc1579695c089ab0ee490db978b1"
   },
   {
     "url": "api.html",
-    "revision": "28c39ad22af641df9b47550543d778bc"
+    "revision": "18988bf2f0d2ed7cf1212ebe3ab4cc97"
   },
   {
     "url": "assets/css/0.styles.56c8a7ca.css",
@@ -36,52 +36,52 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.08c916d0.js",
-    "revision": "f5ad95304fad09cbb547da3254de4a54"
+    "url": "assets/js/10.3c5d417f.js",
+    "revision": "f939077dc1c47d622da9d2d45e78d338"
   },
   {
-    "url": "assets/js/11.3ac90f81.js",
-    "revision": "9e34db5ee81f063f6836b82aa868b383"
+    "url": "assets/js/11.81e95644.js",
+    "revision": "9df1620d423f5dc2251ccc3c1be77304"
   },
   {
-    "url": "assets/js/2.143854f2.js",
-    "revision": "e30ea8a5e0092f96daf9914428a14d60"
+    "url": "assets/js/2.6e116356.js",
+    "revision": "a60d8260772bea19aa8b1566001094ad"
   },
   {
-    "url": "assets/js/3.47ae7a1f.js",
-    "revision": "e1ddfce49b0a3e1b8a316d38b6ee7d65"
+    "url": "assets/js/3.04df3504.js",
+    "revision": "4632c3451d98ed41bc0862d4db78645a"
   },
   {
-    "url": "assets/js/4.fb8d864b.js",
-    "revision": "0903594b3defcea62e61453f97e3cd17"
+    "url": "assets/js/4.ce4f2856.js",
+    "revision": "72b765a546b9a5fac884a70e62f06c83"
   },
   {
-    "url": "assets/js/5.f6a88ef8.js",
-    "revision": "480a5100484dbb989cffd8eb8233c230"
+    "url": "assets/js/5.29ab201e.js",
+    "revision": "80a7ace74fe3a182a1c5dce009e7d580"
   },
   {
-    "url": "assets/js/6.df8e87bd.js",
-    "revision": "e967c97bb29aa0db4270d9377a6b7269"
+    "url": "assets/js/6.58b27e7f.js",
+    "revision": "5716cb2317afb7d56cd8faaf3770d69c"
   },
   {
-    "url": "assets/js/7.97c521c9.js",
-    "revision": "8a7f900621edc95790ac02ece5fc9f80"
+    "url": "assets/js/7.16272338.js",
+    "revision": "319c24603f802f8063826bedbf487d19"
   },
   {
-    "url": "assets/js/8.20791f54.js",
-    "revision": "4b94e2a3ea01b8c58a0bb44493c1e55a"
+    "url": "assets/js/8.f5036b71.js",
+    "revision": "bf22b0dc16f1bf29ae922dc0d26766dc"
   },
   {
-    "url": "assets/js/9.1df9aa87.js",
-    "revision": "7d2a66608e236d54a85b74f840455213"
+    "url": "assets/js/9.fe743c2e.js",
+    "revision": "b61d6ab72bd2cdb744064fde2c3a234e"
   },
   {
-    "url": "assets/js/app.6630a365.js",
-    "revision": "0752afa5c34b72a73df8557cf2dbe9a9"
+    "url": "assets/js/app.00403568.js",
+    "revision": "ec7d5d75eb8cbc8740e6a0fe4a92d647"
   },
   {
     "url": "guide.html",
-    "revision": "ee49679aa9dcc527ed0475e929e7801c"
+    "revision": "5947a13cfed03c56390762abb36751d8"
   },
   {
     "url": "img/logo.png",
@@ -89,7 +89,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "9d8d4c12023f78a618fe6fd7a52dba95"
+    "revision": "0c273c1eaa8d56ff1b77dd33f0d1a8d8"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
